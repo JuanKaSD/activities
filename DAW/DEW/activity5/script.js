8523c1cd12d9592dbc6d94b2266d8f0d24dcf344
juanka <juancarlossantanadominguez@gmail.com>
2018-02-21 21:18:45 +0000

    all finished

--- DAW/DEW/activity5/script.js
+++ DAW/DEW/activity5/script.js
@@ -1,42 +1,51 @@
-var music = ["music1.wav", "music2.wav", "music3.wavº"];
+var music = ["music1.mp3", "music2.mp3", "music3.mp3"];
 var currentPos = 0;
 $(document).ready(function (){
     methods.draw(0);
+    $("#player").attr("src","resources/"+music[currentPos]);
     $("#play").on("click", function (){
         methods.play(music[currentPos]);
     });
     $("#backward").on("click", function (){
         methods.before();        
-        methods.draw(currentPos);
+        methods.play(music[currentPos]);
+        
     });
     $("#fordward").on("click", function (){
         methods.next();
-        methods.draw(currentPos);
+        methods.play(music[currentPos]);
     });    
-    /*snd.on("ended", function() {
-        next();
-        play(music[currentPos]);
-    });*/
+    
+    $("#player").on("ended", function() {
+        methods.next();
+        methods.play(music[currentPos]);
+    });
 });
 var methods = {
     play: function(path){
         path = "resources/"+path;
         document.getElementById("player").play();
-        //$("#player").play();
     },
     draw: function(pos){
-        $("#current").html("Posición actual: "+(pos+1));
+        $("#current").html("Actual song: "+(pos+1));
     },
     next: function(){
         if(currentPos < music.length -1)
             currentPos++;
         else
             currentPos = 0;
+        methods.change(currentPos);
+        methods.draw(currentPos);
     },
     before: function(){
         if(currentPos <= 0)
             currentPos = music.length -1;
         else
             currentPos--;
+        methods.change(currentPos);
+        methods.draw(currentPos);
+    },
+    change: function(currentPos){
+        $("#player").attr("src","resources/"+music[currentPos]);
     }
 }
